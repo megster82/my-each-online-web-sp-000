a94996a017f0ea81b79772916e9e643d8065d689
meaghan jennings <meaghanljennings@gmail.com>
2020-07-22 15:35:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+fruit_collection = ["apples", "oranges", "bananas", "pears"]
+def my_each(fruit_collection) # put argument(s) here
+  puts "I always have #{fruit_collection} in my kitchen."
+  yield(fruit_collection)
+  puts "Now I need to go shopping for #{fruit_collection}."
+  
   while
   
 end
