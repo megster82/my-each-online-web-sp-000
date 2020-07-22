c0c3ea29bfc28010906f8d589d4e95e871d8b6cc
meaghan jennings <meaghanljennings@gmail.com>
2020-07-22 16:08:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,12 @@
-list = ["ab", "cd", "ef"]
+collection = ["blueberries", "strawberries", "oranges"]
 
 def my_each(array)
   i = 0
   while i < array.length
-# The "while" method will always return a value of => nil"
     yield(array[i])
     i += 1
   end
   array
 end
 
-my_each(list) {|i| "I think #{i} are the best fruit!"}
+my_each(collection) {|i| "I think #{i} are the best fruit!"}
