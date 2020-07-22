3c50addf1dfa6643a61553520f53b40b04d654f2
meaghan jennings <meaghanljennings@gmail.com>
2020-07-22 16:01:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,3 +8,16 @@ def my_each(array)
   end
 end
 my_each(collection) {|i| "I think #{i} are the best fruit."}
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
