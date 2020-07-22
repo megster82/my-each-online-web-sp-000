4fb92413a44d476730a5a548818f7caa95d2d480
meaghan jennings <meaghanljennings@gmail.com>
2020-07-22 15:59:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,8 @@ collection = ["apples", "oranges", "bananas"]
 def my_each(array)
   i = 0
   while i < array.length
-  collection << yield(array[i])
+yield(array[i])
     i += 1
   end
-  collection 
 end
 my_each(collection) {|i| "I think #{i} are the best fruit."}
-hello(list) {|i| "Hello #{i} what's up?"}
