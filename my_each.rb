07b8e3393c2be06a2c214efd9a61ac41dca71b1c
meaghan jennings <meaghanljennings@gmail.com>
2020-07-22 15:46:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,12 @@
-collection = [5,12,4,3]
+[5,12,4,3].each do |num|
+  if num !== 0
+    yield num
+    if num == 0 
+      puts "Zero is the final answer."
+  end
+end
+
+    
 def my_each(collection) 
   my_each(collection) do |num|
   yield num
